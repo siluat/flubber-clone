064f0a58456335a8d6b10f00bbf752204a6b16c0
siluat <siluat@gmail.com>
2022-03-21 22:00:10 +0900

    ✨ samePoint, distance

--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,18 @@
-export function isFiniteNumber(number: number) {
+import { Point } from "./types";
+
+export function distance(a: Point, b: Point): number {
+  return Math.sqrt(
+    (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1] * (a[1] - b[1]))
+  );
+}
+
+/**
+ * 두 점이 근사적으로 같은 위치인지 확인
+ */
+export function samePoint(a: Point, b: Point): boolean {
+  return distance(a, b) < 1e-9;
+}
+
+export function isFiniteNumber(number: number): boolean {
   return typeof number === "number" && isFinite(number);
 }
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,4 @@
+export type Point = [number, number];
+
 // x, y 좌표의 점 배열로 고리를 표현
-export type Ring = [number, number][];
+export type Ring = Point[];
